02c7ae47ae2d552f73f82a6034220658773e5ddf
XB <bouteiller.xavier@gmail.com>
2020-10-22 11:07:19 +0200

    sep turn to default and readme modified

--- CaviClean/ParseClass.py
+++ CaviClean/ParseClass.py
@@ -903,5 +903,5 @@ class ParseTreeFolder():
             FileSaveName = input('enter final file name : ') or 'DefaultTable'
             FileSaveName += '.csv'
 
-        self.final_frame.to_csv(FileSaveName,index=False, header=True, sep=';') #which sep ?
+        self.final_frame.to_csv(FileSaveName,index=False, header=True) #which sep ? , sep=';'
         print('saved file {}\n'.format(FileSaveName))
